d8bd56c5b76e3138faadcc734c216d9b5844a0a6
Adro Morelos <me@adro.rocks>
2023-12-11 10:21:00 -0600

    Use a psr18 client discovery as the http client discovery class is deprecated.

--- src/ClientBuilder.php
+++ src/ClientBuilder.php
@@ -8,8 +8,8 @@ use Http\Client\Common\HttpMethodsClient;
 use Http\Client\Common\HttpMethodsClientInterface;
 use Http\Client\Common\Plugin;
 use Http\Client\Common\PluginClientFactory;
-use Http\Discovery\HttpClientDiscovery;
 use Http\Discovery\Psr17FactoryDiscovery;
+use Http\Discovery\Psr18ClientDiscovery;
 use Psr\Http\Client\ClientInterface;
 use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\StreamFactoryInterface;
@@ -23,7 +23,7 @@ final class ClientBuilder
         private ?RequestFactoryInterface $requestFactoryInterface = null,
         private ?StreamFactoryInterface $streamFactoryInterface = null,
     ) {
-        $this->httpClient = $httpClient ?: HttpClientDiscovery::find();
+        $this->httpClient = $httpClient ?: Psr18ClientDiscovery::find();
         $this->requestFactoryInterface = $requestFactoryInterface ?: Psr17FactoryDiscovery::findRequestFactory();
         $this->streamFactoryInterface = $streamFactoryInterface ?: Psr17FactoryDiscovery::findStreamFactory();
     }
